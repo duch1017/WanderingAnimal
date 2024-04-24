8afb237bf1284cf7e87a6fa9f6f174db41e91cf7
duch <1470651260@qq.com>
2024-04-24 14:50:52 +0800

    day01 项目环境搭建

---
+++ WAWeb/src/main/java/com/duch/WaWebApplication.java
@@ -0,0 +1,13 @@
+package com.duch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaWebApplication.class, args);
+    }
+
+}
---
+++ WAWeb/src/test/java/com/duch/WaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
